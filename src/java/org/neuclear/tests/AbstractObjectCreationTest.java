ccb558b4340eb7bc690b4208be855ea65cf1c402
pelle <pelle>
2004-04-02 16:59:04 +0000

    Updated Asset and Asset Builder with semi fully featured functionality. It now has Issuer, Service etc.

--- src/java/org/neuclear/tests/AbstractObjectCreationTest.java
+++ src/java/org/neuclear/tests/AbstractObjectCreationTest.java
@@ -1,12 +1,12 @@
 package org.neuclear.tests;
 
 import org.neuclear.commons.NeuClearException;
-import org.neuclear.commons.crypto.signers.NonExistingSignerException;
 import org.neuclear.id.SignedNamedObject;
 import org.neuclear.id.builders.Builder;
 import org.neuclear.xml.XMLException;
 
 import java.security.GeneralSecurityException;
+import java.security.PublicKey;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,18 +21,34 @@ public abstract class AbstractObjectCreationTest extends AbstractSigningTest {
     }
 
     public void testCreateObject() throws Exception, XMLException {
-        Builder builder=createBuilder();
+        Builder builder = createBuilder();
+//        System.out.println(builder.asXML());
         assertNotNull(builder);
-//        assertTrue(NSTools.isNamedObject(builder.getElement()));
-        SignedNamedObject obj=builder.convert(NAME,getSigner());
+        SignedNamedObject obj = builder.convert(NAME, getSigner());
         assertNotNull(obj);
-        assertEquals(getRequiredClass(),obj.getClass());
+        assertEquals(getRequiredClass(), obj.getClass());
         verifyObject(obj);
     }
 
+    protected void assertEquals(byte a[], byte b[]) {
+        if (a == null && b == null)
+            return;
+        assertFalse("neither is null", a == null || b == null);
+        assertEquals("same length", a.length, b.length);
+        for (int i = 0; i < a.length; i++)
+            assertEquals("content same", a[i], b[i]);
+    }
+
+    protected void assertEqualPublicKeys(PublicKey a, PublicKey b) {
+        assertEquals(a.getClass(), b.getClass());
+        assertEquals(a.getEncoded(), b.getEncoded());
+    }
+
     protected abstract void verifyObject(SignedNamedObject obj) throws Exception;
+
     protected abstract Class getRequiredClass();
+
     protected abstract Builder createBuilder() throws Exception;
 
-    public static final String NAME="test";
+    public static final String NAME = "neu://test";
 }
